1090a1c255764b8051a3f146ebc39fc698765321
Shubi <shubi@campus.technion.ac.il>
2021-02-09 18:23:00 +0200

    add simple predictor

--- basic_predictor.py
+++ basic_predictor.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 
 def fit_predict(res_path):
-    df = pd.read_csv(os.path.join('results', res_path, "df.csv"))
+    df = pd.read_csv(os.path.join('results', res_path, "df_shift.csv"))
     reg = LinearRegression().fit(df.loc[:, 'Wind Speed [m/s]'].values.reshape(-1, 1),
                                  df.loc[:, 'Mean Optical Flow'].values.reshape(-1, 1))
     print(reg.predict([[5]]))
 
 
 if __name__ == '__main__':
-    fit_predict(res_path='09022021_180653')
+    fit_predict(res_path='09022021_181047')
--- video_gradient_example.py
+++ video_gradient_example.py
@@ -14,92 +14,6 @@ from utils import result_visualizer, create_and_configer_logger
 sns.set_theme(color_codes=True)
 
 
-def display_motion(video_path, resize_scale=0.5, levels=60, winsize=4, show_rgb_motion=True, show_gray_motion=True):
-    # The video feed is read in as
-    # a VideoCapture object
-    cap = cv.VideoCapture(video_path)
-
-    # ret = a boolean return value from
-    # getting the frame, first_frame = the
-    # first frame in the entire video sequence
-    _, first_frame = cap.read()
-
-    first_frame = cv.resize(first_frame, dsize=(0, 0), fx=resize_scale, fy=resize_scale)
-    # Converts frame to grayscale because we
-    # only need the luminance channel for
-    # detecting edges - less computationally
-    # expensive
-    prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
-
-    # Creates an image filled with zero
-    # intensities with the same dimensions
-    # as the frame
-    mask = np.zeros_like(first_frame)
-
-    # Sets image saturation to maximum
-    mask[..., 1] = 255
-
-    while cap.isOpened():
-
-        # ret = a boolean return value from getting
-        # the frame, frame = the current frame being
-        # projected in the video
-        _, frame = cap.read()
-        if frame is None:
-            print('End of Video.')
-            break
-
-        frame = cv.resize(frame, dsize=(0, 0), fx=resize_scale, fy=resize_scale)
-        # frame = cv.resize(frame, dim)
-        # Opens a new window and displays the input
-        # frame
-        cv.imshow("input", frame)
-
-        # Converts each frame to grayscale - we previously
-        # only converted the first frame to grayscale
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-        # Calculates dense optical flow by Farneback method
-        flow = cv.calcOpticalFlowFarneback(prev=prev_gray, next=gray,
-                                           flow=None, pyr_scale=0.5, levels=levels, winsize=winsize, iterations=3,
-                                           poly_n=7, poly_sigma=1.2, flags=0)
-
-        # Computes the magnitude and angle of the 2D vectors
-        magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
-
-        # Sets image hue according to the optical flow
-        # direction
-        mask[..., 0] = angle * 180 / np.pi / 2
-
-        # Sets image value according to the optical flow
-        # magnitude (normalized)
-        mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
-
-        # Converts HSV to RGB (BGR) color representation
-        rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
-        h, s, v1 = cv.split(mask)
-
-        # Opens a new window and displays the output frame
-        if show_rgb_motion:
-            cv.imshow("dense optical flow rgb", rgb)
-        if show_gray_motion:
-            cv.imshow("dense optical flow gray", v1)
-
-        # Updates previous frame
-        prev_gray = gray
-
-        # Frames are read by intervals of 1 millisecond. The
-        # programs breaks out of the while loop when the
-        # user presses the 'q' key
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # The following frees up resources and
-    # closes all windows
-    cap.release()
-    cv.destroyAllWindows()
-
-
 def get_stats(video_path, resize_scale=0.5, levels=60, winsize=4, show_rgb_motion=True, show_gray_motion=True,
               num_frames=20):
     # The video feed is read in as
@@ -259,9 +173,6 @@ def run_computations(config) -> pd.DataFrame:
     return df
 
 
-from sklearn.linear_model import LinearRegression
-
-
 def main():
     # config = {'excel_path': 'z6-04349(z6-04349)-1611065058.xlsx',
     #           'video_path': './OneDrive_1_19-01-2021',
@@ -297,10 +208,6 @@ def main():
     df_shift.dropna(inplace=True)
     df_shift.to_csv(os.path.join(save_path, 'df_shift.csv'))
     result_visualizer(df_shift, save_path, shift_str='_shift_5')
-
-    pass
-    print(1)
-    pass
 
 
 if __name__ == '__main__':
